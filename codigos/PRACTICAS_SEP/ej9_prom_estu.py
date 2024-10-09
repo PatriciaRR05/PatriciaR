688301aea020c0578e15089f7a2938e8cbf3f5c5
PatriciaR <patricia.rivadeneyra@ucb.edu.bo>
2024-10-09 18:29:11 -0400

    9deOct

---
+++ codigos/PRACTICAS_SEP/ej9_prom_estu.py
@@ -0,0 +1,18 @@
+suma = 0
+promedio = 0
+cantidad = int(input("Ingrese la cantidad de estudiantes a evaluar: "))
+notas = []
+
+for i in range(cantidad): #en el bucle, de acuerdo a la cantidad se piden las notas 
+    numNotas = int(input("Ingrese la cantidad de notas: "))
+    for q in range(numNotas):
+        nota = int(input("Ingrese la nota del estudiante: "))
+        notas.append(nota) #se agrega las notas ingresadas a la lista 
+
+suma = sum(notas) #se suman las notas para luego sacar el promedio 
+promedio = suma / cantidad
+
+print("Las notas son:", notas)
+print("La suma de las notas es:", suma)
+print("El promedio de las notas es:", promedio)
+#deberia generar una lista para cada estudiante y un promedio para cada estudiante
