3b3f4853b330b2196a7b46d587c969eff31242c8
PatriciaR <patricia.rivadeneyra@ucb.edu.bo>
2024-11-13 18:00:32 -0400

    8Nov

---
+++ codigos/PRACTICAS_SEP/ej22_tkinter.py
@@ -0,0 +1,48 @@
+import tkinter as tk
+import csv
+
+ventana = tk.Tk()
+ventana.title("MiPrimerVentana")
+ventana.geometry("500x500")
+
+#para crear la etiqueta
+nombreContacto=tk.Entry(ventana,text="Nombre")
+nombreContacto.pack()
+
+#para crear la etiqueta
+telefonoContacto=tk.Entry(ventana,text="Telefono")
+telefonoContacto.pack()
+
+#para crear la etiqueta
+direccionContacto=tk.Entry(ventana,text="Direccion")
+direccionContacto.pack()
+
+#para crear la etiqueta
+edadContacto=tk.Entry(ventana,text="Edad")
+edadContacto.pack()
+
+#sirve para rellenar la entrada
+entradaNombre=tk.Entry(ventana)
+entradaNombre.pack()
+entradaTelefono=tk.Entry(ventana)
+entradaTelefono=tk.Entry(ventana)
+
+def guardar():
+    nombre = entradaNombre.get()
+    telefono= telefonoContacto.get()
+    direccion= direccionContacto.get()
+    edad = edadContacto.get()
+    with open("agenta.csv", "a", nweline="")as archivo:
+        contacto=csv.writer(archivo)
+        contacto.writerow([nombre, telefono, edad, direccion])
+
+
+
+
+
+botonGuardar=tk.Button(ventana, text="Guardar contactos", command=guardar)
+botonGuardar.pack()
+
+
+
+ventana.mainloop()
